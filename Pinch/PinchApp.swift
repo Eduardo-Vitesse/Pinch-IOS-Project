6a7cd64408ad6fa6824752ef6f484fc690fb0a93
Eduardo-Vitesse <edu.dev.py@gmail.com>
2022-05-17 00:33:35 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Luiz Eduardo on 17/05/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
